665479e947031142acf23c82080d0d9c7c9c9f72
Veronica-Bulimo <69415192+Veronica-Bulimo@users.noreply.github.com>
2020-08-09 11:30:59 +0300

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 app.config['MYSQL_HOST'] = '127.0.0.1'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'Covid-19-TracingApp'
+app.config['MYSQL_DB'] = 'Covid-19-Contact-Tracing'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 # init MYSQL
 mysql = MySQL(app)
